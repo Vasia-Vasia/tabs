6dd0d4fb5bf68b6a14875353e4732410170449fa
Vasia <34076640+Vasia-Vasia@users.noreply.github.com>
2025-04-28 19:57:47 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../style.css';
+import '../assets/img/html.png';
+import '../assets/img/css.png';
+import '../assets/img/js.png';
+import '../assets/fonts/Raleway-Regular.ttf';
+import '../assets/fonts/Raleway-Bold.ttf';
