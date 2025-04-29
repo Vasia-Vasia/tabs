30db517d2ec3cf05b2077bb40e23a59f3f66a3d2
Vasia <34076640+Vasia-Vasia@users.noreply.github.com>
2025-04-29 19:19:51 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import '../style.css';
-import '../assets/img/html.png';
-import '../assets/img/css.png';
-import '../assets/img/js.png';
-import '../assets/fonts/Raleway-Regular.ttf';
-import '../assets/fonts/Raleway-Bold.ttf';
